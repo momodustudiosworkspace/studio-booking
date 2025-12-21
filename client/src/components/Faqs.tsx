1996b859c62c0752beedccac40db029231d7f109
Ekong Emmanuel <ekonge903@gmail.com>
2025-12-21 18:15:53 +0100

    fix(Bookings): Booking calendar display of accurate date and time slot available

--- client/src/components/Faqs.tsx
+++ client/src/components/Faqs.tsx
@@ -42,7 +42,7 @@ const Faqs = (): React.JSX.Element => {
     },
   ];
   return (
-    <div className='flex flex-col sm:flex-row sm:justify-between mx-auto w-full max-w-7xl'>
+    <div className='flex flex-col sm:flex-row sm:justify-between mx-auto w-full px-4 max-w-7xl'>
       <div className='sm:w-[30%] sm:pl-5'>
         <SectionHeader
           badgeText='FAQs'
