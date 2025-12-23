4c454574139d300f93368cc329993a56877826d9
Ekong Emmanuel <ekonge903@gmail.com>
2025-12-24 00:08:38 +0100

    update(UI): Add home to navbar links

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -332,6 +332,12 @@ export default function Navbar() {
           <div className='mt-6 flow-root'>
             <div className='-my-6 divide-y divide-white/10'>
               <div className='space-y-2 py-6'>
+                <Link
+                  href='/web'
+                  className='-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5'
+                >
+                  Home
+                </Link>
                 <Disclosure as='div' className='-mx-3'>
                   <DisclosureButton className='group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-white hover:bg-white/5'>
                     Product
